6ebe2ad82dff8fa1d4f2f0150a0113056500f2ea
bmlsj <gaga2353@gmail.com>
2022-02-26 23:00:52 +0900

    조건문

--- basic/ConditionalStatement.py
+++ basic/ConditionalStatement.py
@@ -1,25 +1,25 @@
 ## if 문
 
-# weather = input("오늘 날씨는? ")
-# if weather == "비" or weather == "눈":
-#     print("우산을 챙기세요")
+weather = input("오늘 날씨는? ")
+if weather == "비" or weather == "눈":
+    print("우산을 챙기세요")
 
-# elif weather == "미세먼지":
-#     print("마스크를 챙기세요")
+elif weather == "미세먼지":
+    print("마스크를 챙기세요")
 
-# else:
-#     print("준비물 필요 없어요")
+else:
+    print("준비물 필요 없어요")
 
 
-# temp = int(input("오늘 기온은? "))
-# if temp >= 30 :
-#     print("너무 더워요")
-# elif 30 > temp >= 10:
-#     print("괜찮은 날씨에요")
-# elif 10 > temp >= 0:
-#     print("외투를 챙기세요")
-# else:
-#     print("너무 추워요.")
+temp = int(input("오늘 기온은? "))
+if temp >= 30 :
+    print("너무 더워요")
+elif 30 > temp >= 10:
+    print("괜찮은 날씨에요")
+elif 10 > temp >= 0:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요.")
 
 
 
